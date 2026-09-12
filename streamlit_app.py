60715cdaf9593c748ee04f08dc80357b1db7ca90
dedlepexa <Alkekss@yandex.ru>
2026-09-12 14:26:23 +0700

    Update Streamlit app title and add greeting feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("hihihjatest")
-st.write(
-    "123"
-)
+st.title("Моё первое приложение")
+st.write("Привет! Это тестовое приложение на Streamlit.")
+
+name = st.text_input("Введите ваше имя:")
+if name:
+    st.success(f"Здравствуйте, {name}!")
